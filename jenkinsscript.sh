a5626e586b087255e20c3e79cb9d5344ba6b5526
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-10-10 14:55:54 -0400

    Create jenkinsscript.sh

---
+++ jenkinsscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y fontconfig openjdk-17-jre
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y jenkins
